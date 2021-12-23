8abcfe1aead056290dfd3e9071773e4ccf0539f6
Maryna Shyta <marinka.shita@gmail.com>
2021-12-23 16:57:25 +0200

    basic structure commit

--- src/index.php
+++ src/index.php
@@ -1,1 +1,3 @@
-<?php phpinfo();
+<?php include("views/layouts/header.php"); ?>
+<?php include("views/layouts/footer.php"); ?>
+<?php include("views/pages/home.php"); ?>
---
+++ src/views/pages/home.php
@@ -0,0 +1,16 @@
+<div>
+    Natoque dignissim, cursus justo sapien
+    libero nostra accumsan. Quam duis ac mus condimentum.
+    Facilisis lobortis nulla sit ultrices molestie mattis
+    convallis montes consectetur orci iaculis. Euismod morbi eget platea aptent
+    ligula congue tempus aptent at suspendisse. Felis feugiat consequat vel gravida suscipit
+    pellentesque mus lobortis placerat interdum. Eros conubia maecenas proin placerat est lacus
+    mauris nibh nunc scelerisque. Dictumst erat et elementum, duis est vitae gravida est dui. Elit leo, scelerisque.
+</div>
+<div>
+    Facilisis lobortis nulla sit ultrices molestie mattis
+    convallis montes consectetur orci iaculis. Euismod morbi eget platea aptent
+    ligula congue tempus aptent at suspendisse. Felis feugiat consequat vel gravida suscipit
+    pellentesque mus lobortis placerat interdum. Eros conubia maecenas proin placerat est lacus
+    mauris nibh nunc scelerisque. Dictumst erat et elementum, duis est vitae gravida est dui. Elit leo, scelerisque.
+</div>
