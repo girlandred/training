bc2d293443f1cf3a91c6025d07638c51731ba111
Ivan <beerman1988@gmail.com>
2021-12-28 16:33:32 +0200

    Update constants.php

--- src/core/constants.php
+++ src/core/constants.php
@@ -1,4 +1,12 @@
 <?php
 define('ROOT', $_SERVER['DOCUMENT_ROOT']);
-define('VIEW_PAGES_DIR', ROOT . '/views/pages/');
-define('VIEW_LAYOUTS_DIR', ROOT . '/views/layouts/');
+define('CORE', ROOT . '/core');
+define('HENDLERS', ROOT . '/handlers');
+define('VIEW_DIR', ROOT . '/views');
+define('VIEW_PAGES_DIR', ROOT . '/views/pages');
+define('VIEW_LAYOUTS_DIR', ROOT . '/views/layouts');
+
+define('DB_HOST', 'database');
+define('DB_USER', 'myuser');
+define('DB_PASS', 'pass');
+define('DB_NAME', 'mydb');
