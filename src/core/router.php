71cde49446d4c4d029c2d5014076066277aadf79
Ivan <beerman1988@gmail.com>
2021-12-28 16:34:21 +0200

    Update router.php

--- src/core/router.php
+++ src/core/router.php
@@ -2,30 +2,29 @@
 function getRoute()
 {
     $route = trim($_SERVER['REQUEST_URI'], '/');
-    $path = VIEW_PAGES_DIR;
     switch ($route) {
         case '':
         case 'home':
             $view = 'home';
             break;
         case 'create':
-            include('handlers/create.php');
+            require(HENDLERS . '/create.php');
             $view = 'create';
             break;
         case 'show':
-            include('handlers/create.php');
+            require(HENDLERS . '/show.php');
             $view = 'show';
             break;
         default:
             $view = 'error';
     }
-    if (file_exists($path . $view . '.php')) {
-        return VIEW_PAGES_DIR . $view . '.php';
+    if (file_exists(VIEW_PAGES_DIR . '/' . $view . '.php')) {
+        return VIEW_PAGES_DIR . '/' . $view . '.php';
     }
-    return VIEW_PAGES_DIR . $view . '.php';
+    return VIEW_PAGES_DIR . '/' . $view . '.php';
 }
 
 function loadPage()
 {
-    require getRoute();
+    require(getRoute());
 }
