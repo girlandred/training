da09ebe2820e55ad684c31d324264d48a4e1f5d8
Maryna Shyta <marinka.shita@gmail.com>
2021-12-28 17:01:28 +0200

    Fixed constants.php

--- src/core/constants.php
+++ src/core/constants.php
@@ -3,8 +3,8 @@ define('ROOT', $_SERVER['DOCUMENT_ROOT']);
 define('CORE', ROOT . '/core');
 define('HENDLERS', ROOT . '/handlers');
 define('VIEW_DIR', ROOT . '/views');
-define('VIEW_PAGES_DIR', ROOT . '/views/pages');
-define('VIEW_LAYOUTS_DIR', ROOT . '/views/layouts');
+define('VIEW_PAGES_DIR', ROOT . '/views/pages/');
+define('VIEW_LAYOUTS_DIR', ROOT . '/views/layouts/');
 
 define('DB_HOST', 'database');
 define('DB_USER', 'myuser');
