8b9bd4131b65bc42a0bc9014ddad9400e372cad3
Maryna Shyta <marinka.shita@gmail.com>
2021-12-28 18:34:16 +0200

    Created error.php

--- src/views/pages/error.php
+++ src/views/pages/error.php
@@ -1,3 +1,17 @@
-<div>
-    error page
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 align-self-center">
+            <div class="error">
+                <h1>
+                    404 NOT FOUND
+                </h1>
+                <p>
+                    The page you are looking for does not exist.
+                    But you can click the button below
+                    to go back to the homepage.
+                </p>
+                <a class="btn btn-primary" href="home" role="button">Home</a>
+            </div>
+        </div>
+    </div>
 </div>
