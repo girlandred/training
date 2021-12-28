30810cc64908294b7f2d4291bbaed7ccab8d83fa
Ivan <beerman1988@gmail.com>
2021-12-28 11:03:26 +0200

    Update index.php
    use constants

--- src/index.php
+++ src/index.php
@@ -1,9 +1,7 @@
 <?php
-include('views/layouts/header.php');
 require_once('constants.php');
 require('router.php');
-$view = getRoute();
-require($view);
-// require(VIEW_LAYOUTS_DIR);
-// require($layout);
-include('views/layouts/footer.php');
+
+include(VIEW_LAYOUTS_DIR . 'header.php');
+loadPage();
+include(VIEW_LAYOUTS_DIR . 'footer.php');
