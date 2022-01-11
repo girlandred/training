b43aa3ba265071ed0d8ef0c8707539e48813ed13
Maryna Shyta <marinka.shita@gmail.com>
2022-01-11 16:30:26 +0200

    modified delete.js, dynamical deletion

--- src/assets/js/delete.js
+++ src/assets/js/delete.js
@@ -1,22 +1,21 @@
 $(document).ready(function($) {
     $('body').on('click', '.delete', function() {
-        if (confirm("Delete Record?") == true) {
-            var id = $(this).data('id');
-            $.ajax({
-                type: "POST",
-                url: "/show",
-                data: {
-                    id: id,
-                    type: 'delete'
-                },
-                success: function(res) {
-                    $('#title').html(res.title);
-                    $('#vendor').html(res.vendor);
-                    $('#country').html(res.country);
-                    $('#quantity').html(res.quantity);
-                    window.location.reload(true);
-                }
-            });
-        }
+        var id = $(this).data('id');
+        $.ajax({
+            type: 'POST',
+            url: '/show',
+            data: {
+                id: id,
+                type: 'delete'
+            },
+            dataType: 'html',
+            success: function(res) {
+                $('#title').html(res.title);
+                $('#vendor').html(res.vendor);
+                $('#country').html(res.country);
+                $('#quantity').html(res.quantity);
+                window.location.reload(true);
+            }
+        });
     });
 });
