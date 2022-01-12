48f7c7d77c14de6728851e9bfd671da4a0078e32
Maryna Shyta <marinka.shita@gmail.com>
2022-01-12 13:23:26 +0200

    Pagination not working

--- src/assets/js/paginate.js
+++ src/assets/js/paginate.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
+    $("#content").load("show/1");
     $(".page-link").click(function() {
         var id = $(this).attr("data-id");
         var select_id = $(this).parent().attr("id");
         $.ajax({
+            url: "/show",
             type: "GET",
             data: {
                 page: id
             },
             cache: false,
             success: function(dataResult) {
+                $("#content").html(dataResult);
                 $(".pageitem").removeClass("active");
                 $("#" + select_id).addClass("active");
             }
--- src/handlers/show.php
+++ src/handlers/show.php
@@ -10,19 +10,15 @@ if (isset($_POST['type']) && $_POST['type'] === 'delete') {
     } else {
         echo 'Error: ' . $sql . '' . mysqli_error($conn);
     }
-} elseif ($id) {
-    $limit = 5;
+} else {
     $user_id = $_SESSION['id_user'];
+    $limit = 5;
     if (isset($_GET["page"])) {
         $page  = $_GET["page"];
     } else {
         $page = 1;
     };
     $start_from = ($page - 1) * $limit;
-    $sql = "SELECT * FROM products WHERE id='$id' AND user_id='$user_id' LIMIT $start_from, $limit";
-    $result = mysqli_query($conn, $sql);
-} else {
-    $user_id = $_SESSION['id_user'];
-    $sql = "SELECT * FROM products WHERE user_id='$user_id'";
+    $sql = "SELECT * FROM products WHERE user_id='$user_id' LIMIT $start_from, $limit";
     $result = mysqli_query($conn, $sql);
 }
